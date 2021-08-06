b8d868f8e28db7b139b6539586542e6ce4273641
Fandrew44 <ahgreeting@gmail.com>
2021-08-06 00:05:22 -0600

    initialize experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import PortfolioContext from '../../context/context';
+import Title from '../Title/Title';
+
+const Experience = () => {
+    const [isDesktop, setIsDesktop] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+  
+    useEffect(() => {
+      if (window.innerWidth > 769) {
+        setIsDesktop(true);
+        setIsMobile(false);
+      } else {
+        setIsMobile(true);
+        setIsDesktop(false);
+      }
+    }, []);
+
+    return(
+
+    );
+}
