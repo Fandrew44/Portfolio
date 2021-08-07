00b4c3dbfd372f620c5cadeeba2efb264a4a3bdf
Fandrew44 <ahgreeting@gmail.com>
2021-08-07 15:51:38 -0600

    initialize experience component structure

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
+import Tilt from 'react-tilt';
 import { Container, Row, Col } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
+import ProjectImg from '../Image/ProjectImg';
 
 const Experience = () => {
   const { experiences } = useContext(PortfolioContext);
@@ -26,11 +28,66 @@ const Experience = () => {
         <div className="experience-wrapper">
           <Title title="Experience" />
           {experiences.map((experience) => {
-            const { id, logo, organization, position, descripton, url } = experience;
+            const { id, logo, organization, position, description, url } = experience;
 
             return (
-              <p1>Hello</p1>
-
+              <Row key={id}>
+                <Col lg={4} sm={12}>
+                  <Fade
+                    left={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={500}
+                    distance="30px"
+                  >
+                    <div className="experience-wrapper__text">
+                      <h3 className="experience-wrapper__text-organization">{organization || 'Organization Name'}</h3>
+                      <h2 className="experience-wrapper__text-position">{position | 'Position title'}</h2>
+                      <div>
+                        <p>
+                          {description || 'Job Description'}
+                        </p>
+                      </div>
+                    </div>
+                  </Fade>
+                </Col>
+                <Col lg={8} sm={12}>
+                  <Fade
+                    right={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={1000}
+                    distance="30px"
+                  >
+                    <div className="project-wrapper__image">
+                      <a
+                        href={url || '#!'}
+                        target="_blank"
+                        aria-label="Organization Link"
+                        rel="noopener noreferrer"
+                      >
+                        <Tilt
+                          options={{
+                            reverse: false,
+                            max: 8,
+                            perespective: 1000,
+                            scale: 1,
+                            speed: 300,
+                            transition: true,
+                            axis: null,
+                            reset: true,
+                            easing: 'cubic-bezier(.03,.98,.52,.99)',
+                          }}
+                        >
+                          <div data-tilt className="thumbnail rounded">
+                            <ProjectImg alt={organization} fileName={logo}></ProjectImg>
+                          </div>
+                        </Tilt>
+                      </a>
+                    </div>
+                  </Fade>
+                </Col>
+              </Row>
             );
           })};
         </div>
